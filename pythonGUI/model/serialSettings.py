dfc9112c0a793c6d88af3c49650e170cb23c052e
Sean <s.l.visser@st.hanze.nl>
2018-11-06 20:08:21 +0100

    Default select + DropDown fix
    Dropdown krijgt nu als standaart selected de waarde uit de dictionary.
    
    Er moet nog een functie komen om de dictionary te updaten :3

--- pythonGUI/model/serialSettings.py
+++ pythonGUI/model/serialSettings.py
@@ -14,6 +14,7 @@ timeout = 1
 #Rolluik bediening settings
 rolluikPoort = ['COM4', 'COM5', 'COM6', 'COM7', 'COM8']
 rolluikNaam = ['Eenheid 1', 'Eenheid 2', 'Puppies <3', 'Eenhied Daddy', 'TeamDaddy']
+rolluikDict = {1:'COM4',2:'COM5',3:'COM6',4:'COM7',5:'COM8'}
 
 #Rolluik status
 status = ['green', 'green','green','red','green']
--- pythonGUI/view/instellingen.py
+++ pythonGUI/view/instellingen.py
@@ -26,12 +26,27 @@ class instellingGUI(Frame):
         label = Label(self, text="Instellingen", font=LARGE_FONT, background="white")
         label.grid(row=0, column=1,pady=15)
         
+        sesam.scanCOM()
         
         varDrop1 = StringVar(self)
+        varDrop1.set(serialSettings.rolluikDict.get(1))
+        
         varDrop2 = StringVar(self)
+        varDrop2.set(serialSettings.rolluikDict.get(2))
+        
         varDrop3 = StringVar(self)
+        varDrop3.set(serialSettings.rolluikDict.get(3))
+       
         varDrop4 = StringVar(self)
+        varDrop4.set(serialSettings.rolluikDict.get(4))
+        
         varDrop5 = StringVar(self)
+        varDrop5.set(serialSettings.rolluikDict.get(5))
+ 
+        default = serialSettings.rolluikDict.get(1)
+        
+        print(serialSettings.rolluikDict.values())
+        
         rowI = 5
         for rolluik in range(len(serialSettings.rolluikNaam)):
             
@@ -54,19 +69,24 @@ class instellingGUI(Frame):
         knop1.grid(row=110, column=1) 
         
         #Dropdown menu met comporten
-        comports = ttk.OptionMenu(self,varDrop1, *serialSettings.rolluikPoort)
+        comports = ttk.OptionMenu(self,varDrop1, *serialSettings.rolluikDict.values())
         comports.grid(column=1, row=5, pady=5, padx=5, ipadx=155)
+        varDrop1.set(serialSettings.rolluikDict.get(1))
         
-        comport1 = ttk.OptionMenu(self,varDrop2, *serialSettings.rolluikPoort)
+        comport1 = ttk.OptionMenu(self,varDrop2, *serialSettings.rolluikDict.values())
         comport1.grid(column=1, row=6, pady=5, padx=5, ipadx=155)
+        varDrop2.set(serialSettings.rolluikDict.get(2))
         
-        comport2 = ttk.OptionMenu(self,varDrop3, *serialSettings.rolluikPoort)
+        comport2 = ttk.OptionMenu(self,varDrop3, *serialSettings.rolluikDict.values())
         comport2.grid(column=1, row=7, pady=5, padx=5, ipadx=155)
+        varDrop3.set(serialSettings.rolluikDict.get(3))
         
-        comport3 = ttk.OptionMenu(self,varDrop4, *serialSettings.rolluikPoort)
+        comport3 = ttk.OptionMenu(self,varDrop4, *serialSettings.rolluikDict.values())
         comport3.grid(column=1, row=8, pady=5, padx=5, ipadx=155)
+        varDrop4.set(serialSettings.rolluikDict.get(4))
         
-        comport4 = ttk.OptionMenu(self,varDrop5, *serialSettings.rolluikPoort)
+        comport4 = ttk.OptionMenu(self,varDrop5, *serialSettings.rolluikDict.values())
         comport4.grid(column=1, row=9, pady=5, padx=5, ipadx=155)
+        varDrop5.set(serialSettings.rolluikDict.get(5))
         
         print(serialSettings.rolluikPoort)
