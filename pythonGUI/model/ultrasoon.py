d26a091059a9ca242822af06a9c77192f5521b35
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-09 14:38:14 +0100

    Ultrasoon
    Ultrasoon shit, en een frame gemaakt voor Daan & Mark om bezig te gaan met statistiek =)

---
+++ pythonGUI/model/ultrasoon.py
@@ -0,0 +1,25 @@
+'''
+Created on 4 Nov 2018
+
+@author: seanv
+
+Hihi, t was al af xd
+
+
+'''
+import serial
+from model import serialSettings as settings
+from model import rolluik as sesam
+
+def getUltrasoonArduino(rolluik):
+    comport=settings.rolluikDict.get(rolluik)
+    serial = serial.Serial(comport= comport, baudrate = settings.baudrate, timeout = settings.timeout)
+    serial.write(b'd') #Moet ook licht zijn in C-code
+    ultrasoon = serial.read() #of readline().decode('ascii') als er ascii gebruikt is =)
+    return ultrasoon
+
+def getUltrasoon(rolluik):
+    return settings.ultra[rolluik-1]
+
+def updateUltrasoon(rolluik):
+    settings.ultra[rolluik-1] = getUltrasoonArduino(rolluik)
