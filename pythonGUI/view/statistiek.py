98f32dea9273caf21cbe613aa32e62f4a0626cf8
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-11-11 21:08:44 +0100

    Deprecated functie vervangen
    Deprecated functie vervangen door niet-deprecated functie

--- pythonGUI/view/statistiek.py
+++ pythonGUI/view/statistiek.py
@@ -66,9 +66,8 @@ class statistiekGUI(Frame):
         a.bar([1,2,3,4,5],[serialSettings.temp[0],serialSettings.temp[1],serialSettings.temp[2],serialSettings.temp[3],serialSettings.temp[4]],color='orange')       
 
         canvas = FigureCanvasTkAgg(f,self)
-        canvas.show()
         canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH,expand=True)
-        canvas.show()
+        canvas.draw()
             
         print("Hello")
            
